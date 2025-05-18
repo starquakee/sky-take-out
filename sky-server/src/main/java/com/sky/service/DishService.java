4f05477245de130e1f16396f21aa766b04928b2e
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-18 19:34:47 +0800

    Add product browsing for users

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,8 @@ public interface DishService {
 
     void updateWithFlavor(DishDTO dishDTO);
 
-    List<DishVO> list(Long categoryId);
+    List<Dish> list(Long categoryId);
+
+    List<DishVO> listWithFlavor(Dish dish);
+
 }
