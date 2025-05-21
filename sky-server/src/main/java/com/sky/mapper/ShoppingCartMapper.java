71acd7c963205a359ec54d9a26454da81033a61c
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-22 00:12:24 +0800

    Add shoppingCart

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart cart);
+
+    @Insert("insert into shopping_cart(user_id,dish_id,setmeal_id,dish_flavor,name,image,amount,number,create_time)" +
+            " values (#{userId},#{dishId},#{setmealId},#{dishFlavor},#{name},#{image},#{amount},#{number},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
