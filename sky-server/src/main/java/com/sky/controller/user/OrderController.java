34edd689cb76b9f7f7f2260a4cb9c93acac6d404
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-24 15:22:53 +0800

    Add submit order

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Api(tags = "用户订单管理")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+    @PostMapping("/submit")
+    @ApiOperation("用户提交订单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用户提交订单:{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submit(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+
+}
